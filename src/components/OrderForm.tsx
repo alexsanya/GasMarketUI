1a93ab19746bcd5ecfad02b421d876fcbb8672ad
Oleksandr Koval <alexsanyakoval@gmail.com>
2023-10-21 12:09:53 -0500

    refactoring

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -1,16 +1,40 @@
-'use client'
+import { useState, useRef } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
-import { useState } from 'react'
-import PermitMessageSigner from '../components/PermitMessageSigner'
-import RewardMessageSigner from '../components/RewardMessageSigner'
+import PermitMessageSigner from './PermitMessageSigner'
+import RewardMessageSigner from './RewardMessageSigner'
+
 
 export function OrderForm() {
-  const [token, setToken] = useState('')
-  const [reward, setReward] = useState('')
-  const [value, setValue] = useState('')
-  const [lifetime, setLifetime] = useState('')
+
+  const [orderData, setOrderData] = useState(null)
   const [permitSignature, setPermitSignature] = useState('')
-  const [permitMessage, setPermitMessage] = useState({})
+  const [permitMessage, setPermitMessage] = useState(null)
+
+  const orderForm = useRef();
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+
+    setOrderData({
+      token: data.get('token'),
+      value: data.get('value'),
+      reward: data.get('reward'),
+      lifetime: data.get('lifetime')
+    })
+
+  };
 
   const onPermitSigned = (message, signature) => {
     setPermitMessage(message)
@@ -18,6 +42,7 @@ export function OrderForm() {
   }
 
   const onRewardSigned = async (message, rewardSignature) => {
+    const { token, value, reward } = orderData
     const order = {
       signer: permitMessage.owner,
       token,
@@ -27,52 +52,91 @@ export function OrderForm() {
       permitSignature,
       rewardSignature
     }
-    const response = await fetch('/api/order', {
+    await fetch('/api/order', {
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(order),
     })
-
+    orderForm.current.reset()
+    setOrderData(null)
+    setPermitMessage(null)
+    setPermitSignature('')
   }
 
+
   return (
-    <>
-      <div>
-        Token address:{' '}
-        <input
-          onChange={(e) => setToken(e.target.value)}
-          placeholder="token address"
-          value={token}
-        />
-      </div>
-      <div>
-        Value:{' '}
-        <input
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="value"
-          value={value}
-        />
-      </div>
-      <div>
-        Reward:{' '}
-        <input
-          onChange={(e) => setReward(e.target.value)}
-          placeholder="reward"
-          value={reward}
-        />
-      </div>
-      <div>
-        Lifetime in seconds:{' '}
-        <input
-          onChange={(e) => setLifetime(e.target.value)}
-          placeholder="lifetime"
-          value={lifetime}
-        />
-      </div>
-      <PermitMessageSigner token={token} value={value} lifetime={lifetime} onSuccess={onPermitSigned} />
-      <RewardMessageSigner permitSignature={permitSignature} value={reward} onSuccess={onRewardSigned}/>
-    </>
+    <Container component="main" maxWidth="xs">
+      { orderData && <>
+        <PermitMessageSigner token={orderData.token} value={orderData.value} lifetime={orderData.lifetime} onSuccess={onPermitSigned} />
+        <RewardMessageSigner permitSignature={permitSignature} value={orderData.reward} onSuccess={onRewardSigned} />
+        </>
+      }
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Make an order
+        </Typography>
+        <Box ref={orderForm} component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="token"
+            label="Token address"
+            name="token"
+            autoComplete="token"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="token"
+            label="Value"
+            name="value"
+            autoComplete="value"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="reward"
+            label="Reward"
+            name="reward"
+            autoComplete="value"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="lifetime"
+            label="Lifetime"
+            name="lifetime"
+            autoComplete="value"
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign and publish
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   )
 }
+
--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,21 +1,29 @@
+
 import '@rainbow-me/rainbowkit/styles.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { OrderForm } from '../../components/OrderForm'
+
 
 import { ConnectButton } from '../../components/ConnectButton'
 import { Connected } from '../../components/Connected'
 import { Providers } from '../../app/providers'
-import { OrderForm } from '../../components/OrderForm'
 
-export function Order() {
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+
   return (
-    <Providers>
-      <ConnectButton />
+    <ThemeProvider theme={defaultTheme}>
+      <Providers>
+        <ConnectButton />
+        <Connected>
+          <OrderForm />
+        </Connected>
+      </Providers>
 
-      <Connected>
-        <h2>Order details</h2>
-        <OrderForm />
-      </Connected>
-    </Providers>
-  )
+      
+    </ThemeProvider>
+  );
 }
-
-export default Order
