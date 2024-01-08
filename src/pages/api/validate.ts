2fbc396435c0a34397509aeff9051e22d86d87fe
Oleksandr Koval <alexsanyakoval@gmail.com>
2024-01-07 20:10:18 -0500

    Add endpoint to validate orders

---
+++ src/pages/api/validate.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import validator from '../../services/validator'
+
+type ResponseData = {
+  isValid: boolean
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method !== 'POST') {
+    return res.status(404)
+  }
+  const { isValid } = await validator.validate(req.body)
+  res.status(200).json({ isValid })
+}
